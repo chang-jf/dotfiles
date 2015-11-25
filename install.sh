f57bc824ef965211da7e45efb1e629b863407c0f
Chang, Jung-Feng <mr.chang.jf@gmail.com>
2015-11-25 13:39:08 +0800

    installation script to link dotfiles into ~/

---
+++ install.sh
@@ -0,0 +1,30 @@
+# -------------------------------------------------------------------
+# ~/.dotfiles/install.sh
+#
+# This file is executed for initial my dotfiles by softlink them into home directory.
+# -------------------------------------------------------------------
+#!/bin/bash
+# -------------------------------------------------------------------
+# Initial submodules
+# -------------------------------------------------------------------
+git submodule init ~/.dotfiles/bash/dircolors-solarized
+git submodule update
+ln -sf ~/.dotfiles/bash/dircolors-solarized/dircolors.256dark ~/.dir_colors
+
+# -------------------------------------------------------------------
+# bash rc files
+# -------------------------------------------------------------------
+ln -sf ~/.dotfiles/bash/bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/bash/bashrc ~/.bashrc
+ln -sf ~/.dotfiles/bash/inputrc ~/.inputrc
+ln -sf ~/.dotfiles/bash/bin ~/.bin
+
+# -------------------------------------------------------------------
+# lftp rc files
+# -------------------------------------------------------------------
+ln -sf ~/.dotfiles/lftp/lftprc ~/.lftprc
+
+# -------------------------------------------------------------------
+# git config files
+# -------------------------------------------------------------------
+ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
