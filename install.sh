79ac80fac7c55a5181698a259cc8e517c412dee1
Chang, Jung-Feng <mr.chang.jf@gmail.com>
2015-12-16 14:33:10 +0800

    make install.sh able to run from anywhere

--- install.sh
+++ install.sh
@@ -1,56 +1,68 @@
 # -------------------------------------------------------------------
-# ~/.dotfiles/install.sh
+# $DOTFILES_DIR/install.sh
 #
 # This file is executed for initial my dotfiles by softlink them into home directory.
 # -------------------------------------------------------------------
-#!/bin/bash
+#!/usr/bin/env bash
 # -------------------------------------------------------------------
+# -------------------------------------------------------------------
+# Get current dir (so run this script from anywhere)
+# -------------------------------------------------------------------
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # -------------------------------------------------------------------
-# Install default hand tools
+# Update dotfiles itself first
 # -------------------------------------------------------------------
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # -------------------------------------------------------------------
-# Initial submodules
+# Then submodules
 # -------------------------------------------------------------------
-git submodule init ~/.dotfiles/bash/dircolors-solarized
+#DOTFILES_DIR:/Users/jfc/.dotfiles
+git submodule init $DORFILES_DIR/bash/dircolors-solarized
 git submodule update
-ln -sf ~/.dotfiles/bash/dircolors-solarized/dircolors.256dark ~/.dir_colors
+ln -sf $DOTFILES_DIR/bash/dircolors-solarized/dircolors.256dark ~/.dir_colors
 
 # -------------------------------------------------------------------
 # bash rc files
 # -------------------------------------------------------------------
-ln -sf ~/.dotfiles/bash/bash_profile ~/.bash_profile
-ln -sf ~/.dotfiles/bash/bashrc ~/.bashrc
-ln -sf ~/.dotfiles/bash/inputrc ~/.inputrc
-ln -sf ~/.dotfiles/bash/bin ~/.bin
+ln -sf $DOTFILES_DIR/bash/bash_profile ~/.bash_profile
+ln -sf $DOTFILES_DIR/bash/bashrc ~/.bashrc
+ln -sf $DOTFILES_DIR/bash/inputrc ~/.inputrc
+ln -sf $DOTFILES_DIR/bash/bin ~/.bin
 
 # -------------------------------------------------------------------
 # lftp rc files
 # -------------------------------------------------------------------
-ln -sf ~/.dotfiles/lftp/lftprc ~/.lftprc
+ln -sf $DOTFILES_DIR/lftp/lftprc ~/.lftprc
 
 # -------------------------------------------------------------------
 # git config files
 # -------------------------------------------------------------------
-ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -sf $DOTFILES_DIR/git/gitconfig ~/.gitconfig
 
 # -------------------------------------------------------------------
 # ssh config file
 # -------------------------------------------------------------------
-ln -sf ~/.dotfiles/ssh/config ~/.ssh/config
+ln -sf $DOTFILES_DIR/ssh/config ~/.ssh/config
 
 # -------------------------------------------------------------------
 # tmux rc files
 # -------------------------------------------------------------------
-ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf $DOTFILES_DIR/tmux/tmux.conf ~/.tmux.conf
 
 # -------------------------------------------------------------------
 # wgetrc files
 # -------------------------------------------------------------------
-ln -sf ~/.dotfiles/wget/wgetrc ~/.wgetrc
+ln -sf $DOTFILES_DIR/wget/wgetrc ~/.wgetrc
 
 # -------------------------------------------------------------------
 # curlrc files
 # -------------------------------------------------------------------
-ln -sf ~/.dotfiles/curl/curlrc ~/.curlrc
+ln -sf $DOTFILES_DIR/curl/curlrc ~/.curlrc
+
+# -------------------------------------------------------------------
+# Everything done, let's get out of there.
+# -------------------------------------------------------------------
+exit
