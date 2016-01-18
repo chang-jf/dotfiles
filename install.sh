447293c6cf0e0be4ca36ef6dee2dd285b043a543
Chang, Jung-Feng <mr.chang.jf@gmail.com>
2016-01-18 23:38:23 +0800

    bug fix: re-run install.sh cause ~/.bin soft-link to self

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ echo "# Update dotfiles itself first"
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # -------------------------------------------------------------------
-# Then submodules
+# Then update submodules
 # -------------------------------------------------------------------
 echo "# Then submodules"
 pushd $DOTFILES_DIR
@@ -29,50 +29,44 @@ popd
 ln -sf $DOTFILES_DIR/bash/dircolors-solarized/dircolors.256dark ~/.dir_colors
 
 # -------------------------------------------------------------------
-# bash rc files
+# Soft-link rc files into home
 # -------------------------------------------------------------------
-echo "# bash rc files"
+echo "# soft-link bash rc files"
 ln -sf $DOTFILES_DIR/bash/bash_profile ~/.bash_profile
 ln -sf $DOTFILES_DIR/bash/bashrc ~/.bashrc
 ln -sf $DOTFILES_DIR/bash/inputrc ~/.inputrc
+rm -f ~/.bin
 ln -sf $DOTFILES_DIR/bash/bin ~/.bin
-
-# -------------------------------------------------------------------
-# lftp rc files
-# -------------------------------------------------------------------
-echo "# lftp rc files"
+echo "# soft-link lftp rc files"
 ln -sf $DOTFILES_DIR/lftp/lftprc ~/.lftprc
-
-# -------------------------------------------------------------------
-# git config files
-# -------------------------------------------------------------------
-echo "# gitconfig files"
+echo "# soft-link gitconfig files"
 ln -sf $DOTFILES_DIR/git/gitconfig ~/.gitconfig
-
-# -------------------------------------------------------------------
-# ssh config file
-# -------------------------------------------------------------------
-echo "# ssh config"
+echo "# soft-link ssh config"
 [ ! -d ~/.ssh ] && mkdir ~/.ssh
 ln -sf $DOTFILES_DIR/ssh/config ~/.ssh/config
-
-# -------------------------------------------------------------------
-# tmux rc files
-# -------------------------------------------------------------------
-echo "# tmux rc files"
+echo "# soft-link tmux rc files"
 ln -sf $DOTFILES_DIR/tmux/tmux.conf ~/.tmux.conf
-
-# -------------------------------------------------------------------
-# wgetrc files
-# -------------------------------------------------------------------
-echo "# wget rc files"
+echo "# soft-link wget rc files"
 ln -sf $DOTFILES_DIR/wget/wgetrc ~/.wgetrc
+echo "# soft-link curl rc files"
+ln -sf $DOTFILES_DIR/curl/curlrc ~/.curlrc
 
 # -------------------------------------------------------------------
-# curlrc files
+# Install command line tools
 # -------------------------------------------------------------------
-echo "# curl rc files"
-ln -sf $DOTFILES_DIR/curl/curlrc ~/.curlrc
+# With homebrew(MAC)
+if [ "$(uname)" == "Darwin" ]; then
+    . "$DOTFILES_DIR/install/brew.sh"
+    . "$DOTFILES_DIR/install/brew-cask.sh"
+    . "$DOTFILES_DIR/install/bash.sh"
+    . "$DOTFILES_DIR/install/osx.sh"
+else
+    . "$DOTFILES_DIR/install/osx.sh"
+# Using apt
+# Using yum
+# Using pacman
+# Using emerge
+fi
 
 # -------------------------------------------------------------------
 # Everything done, let's get out of there.
