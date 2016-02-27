e595157a69baba2ae2f2eeadf50d46363a4077bf
Chang, Jung-Feng <mr.chang.jf@gmail.com>
2016-02-27 12:13:28 +0800

    add Thumbs.db and Picasa.ini into target list of trash cleaner

--- bash/bin/cleanup_dsstore.sh
+++ bash/bin/cleanup_dsstore.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Recursively delete `.DS_Store` files
 find . -type f -name '*.DS_Store' -ls -delete
+find . -type f -name 'Thumbs.db' -ls -delete
+find . -type f -name 'Picasa.ini' -ls -delete
