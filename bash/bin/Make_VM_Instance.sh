ee28a9c1638d51a8feb54ad74d5b0ad901f6727b
Chang, Jung-Feng <mr.chang.jf@gmail.com>
2017-02-23 14:35:41 +0800

    add chinese description for eash question

--- bash/bin/Make_VM_Instance.sh
+++ bash/bin/Make_VM_Instance.sh
@@ -25,6 +25,7 @@ die() {
 #Select base template
 #==========================================
 REPLY=''
+echo -e "\n$YELLOW 選擇虛擬機樣本： $NC\n"
 PS3="Select a template for make up instance: "
 select TEMPLATE in `find $QEMU_TEMPLATE_DIR -maxdepth 1 -iname *.img -type f -print|sort`
 do
@@ -35,6 +36,7 @@ do
 
 #Select iso location for make tempalte with
 #==========================================
+echo -e "\n$YELLOW 除了virtio windows驅動CD(預設自動掛載)外，選擇額外掛載的iso file： $NC\n"
 PS3="Select iso to mount(installation CD , application or drivers?): "
 select ISO in `find $ISO_DIR -maxdepth 1 \( -iname *.iso -o -iname *.vfd \) -type f -print|sort`
 do
@@ -50,6 +52,7 @@ ISO_CONFIG=$ISO_CONFIG" -drive file='$VIRTIO_DRIVER',if=ide,index=3,media=cdrom"
 
 #Specify VM name
 #========================
+echo -e "\n$YELLOW 指定虛擬機名稱： $NC\n"
 UNIQUE_INSTANCE_NAME=`date +%M%S%N|md5sum|sed 's/^\(..........\).*$/\1/'`
 [ $AUTO_INSTALL == "TRUE" ] && VM_NAME=$UNIQUE_INSTANCE_NAME || read -e -p "Specify VM name: " -i `basename $TEMPLATE`"_Instance" VM_NAME
 [ -z $VM_NAME ] && die "No VM name specified, aborting..."
@@ -65,6 +68,7 @@ VM_DISK_CONFIG="-drive file='$VM_DISK',if=ide,index=0,media=disk"
 
 #what is memory size?
 #====================
+echo -e "\n$YELLOW 指定分配給虛擬機的記憶體容量： $NC\n"
 [ $AUTO_INSTALL != "TRUE" ] && `read -e -p "Allocate Memory size: " -i $MEM_SIZE MEM_SIZE`
 [ -z $MEM_SIZE ] && die "No memory size specified, aborting..."
 [[ ! $MEM_SIZE =~ ^[0-9bBkKmMgGtT]+$ ]] && die "Invalid memory size, Aborting..." || warn "Set $GREEN[$MEM_SIZE]$NC memory for VM"
@@ -76,6 +80,8 @@ if [ $AUTO_INSTALL == "TRUE" ]
 then
     SOUND_HW=$DEFAULT_SOUND_HW
 else
+    echo -e "\n$YELLOW 選擇音效卡：$NC\n"
+    echo -e "$YELLOW 7) hda    - Windows 7建議使用$NC\n"
     REPLY=''
     PS3="Select a sound card: "
     select SOUND_HW in ${SOUND_HWS[*]}
@@ -93,6 +99,9 @@ if [ $AUTO_INSTALL == "TRUE" ]
 then
     VGA=$DEFAULT_VGA
 else
+    echo -e "\n$YELLOW 選擇顯示卡： $NC\n"
+    echo -e "$YELLOW 2) qxl    - 虛擬機安裝於遠端宿主機選擇qxl驅動以spice連線至虛擬機 $NC"
+    echo -e "$YELLOW 3) cirrus - 本機管理Windows 7虛擬機時$NC\n"
     REPLY=''
     PS3="Select a vga card: "
     select VGA in ${VGAS[*]}
@@ -124,6 +133,9 @@ if [ $AUTO_INSTALL == "TRUE" ]
 then
     NIC=$DEFAULT_NIC
 else
+    echo -e "\n$YELLOW 選擇網路卡： $NC\n"
+    echo -e "$YELLOW 1)  e1000          - 預設 $NC"
+    echo -e "$YELLOW 25) virtio-net-pci - 半虛擬化驅動$NC\n"
     REPLY=''
     PS3="Select a network card: "
     select NIC in ${NICS[*]}
@@ -143,6 +155,10 @@ if [ $AUTO_INSTALL == "TRUE" ]
 then
     NET=$DEFAULT_NET
 else
+    echo -e "\n$YELLOW 選擇網路模式： $NC\n"
+    echo -e "$YELLOW 1) user           - 虛擬機內可以\\10.0.2.4\qemu與宿主機交換檔案，僅支援tcp/udp，無icmp(NATed mode) $NC"
+    echo -e "$YELLOW 2) tap            - 連至default route bridge, 虛擬機與宿主機位於同一網路(bridged mode) $NC"
+    echo -e "$YELLOW 3) bridge         - 連接至指定bridge, bridge需手動設定，可以實作(host only/isolated) $NC\n"
 	REPLY=''
 	PS3="Select a network backend: "
 	select NET in ${NETS[*]}
@@ -153,14 +169,9 @@ fi
 [ -z $NET ] && die "Network backend not selected." || warn "Provision with network backend:$GREEN [$NET] $NC"
 #NET_CONFIG="-net $NET"
 NET_CONFIG="-netdev '$NET',id='netdev-`echo $MAC_ADDR|sed -e 's/://g'`'"
+
 if [ "$NET" == "user" ]
 then
-#    if (mount|grep KVM/utilities>/dev/null); then
-#        echo "already mounted"
-#    else
-#        sudo mount --bind /home/angus/iso /home/angus/KVM/utilities
-#        sudo mount -o remount,ro,bind /home/angus/iso /home/angus/KVM/utilities
-#    fi
     NET_CONFIG="$NET_CONFIG,smb='$QEMU_HOME/utilities'"
 fi
 
