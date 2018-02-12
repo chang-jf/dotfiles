481405b5d03c9cfe2c14812cac8a2632075d0521
Chang, Jung-Feng <mr.chang.jf@gmail.com>
2018-02-12 23:20:49 +0800

    modify for osX

--- install/after_osx.sh
+++ install/after_osx.sh
@@ -9,22 +9,22 @@ brew cleanup; brew cask cleanup
 # Install packages
 
 apps=(
-    bash-completion
-    curl
-    git
-    lftp
+    #bash-completion
+    #curl
+    #git
+    #lftp
     tmux
-    wget
-    vim
-    lynx
-    p7zip
-    python
-    homebrew/binary/rar
-    unrar
+    #wget
+    #vim
+    #lynx
+    #p7zip
+    #python
+    #homebrew/binary/rar
+    #unrar
     reattach-to-user-namespace                               #let tmux support mac's clipboard
-    opencc
-    rsync
-    md5sha1sum
+    #opencc
+    #rsync
+    #md5sha1sum
 )
 
 brew install "${apps[@]}"
@@ -37,13 +37,13 @@ brew install "${apps[@]}"
 #brew cask install google-drive			#the google drive
 brew cask install alfred               		#quick search apps and run
 #brew cask install xmind				#mindmapping tool
-brew cask install appcleaner			#help complete remove mac app
-brew cask install picasa			#picture manager
+#brew cask install appcleaner			#help complete remove mac app
+#brew cask install picasa			#picture manager
 #brew cask install techstoreclub-simple-comic	#comic reader
-brew cask install edgeview                      #comic reader
+#brew cask install edgeview                      #comic reader
 brew cask install vlc                		#video player
 brew cask install filezilla
-brew cask install osxfuse                       #add xfs, ntfs, ext2|3|4 support
+#brew cask install osxfuse                       #add xfs, ntfs, ext2|3|4 support
 
 # Deploy vim rc files
 curl -o - https://raw.githubusercontent.com/chang-jf/vimrc/master/auto-install.sh | sh
