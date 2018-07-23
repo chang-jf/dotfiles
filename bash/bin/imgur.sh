f03aa5cf27ade2d43f58077d55e801746c38370f
Chang, Jung-Feng <mr.chang.jf@gmail.com>
2018-07-23 17:35:36 +0800

    script for publish images on internet and get url via imgur's service

---
+++ bash/bin/imgur.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+imgur() {
+    for i in "$@"; do
+        curl -# -F "image"=@"$i" -F "key"="4907fcd89e761c6b07eeb8292d5a9b2a" imgur.com/api/upload.xml|\
+        grep -Eo '<[a-z_]+>http[^<]+'|sed 's/^<.\|_./\U&/g;s/_/ /;s/<\(.*\)>/\x1B[0;34m\1:\x1B[0m /'
+    done
+}
+
