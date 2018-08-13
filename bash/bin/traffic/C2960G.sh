bc58bc7749d8347e7d11977244943dae42c02604
Chang, Jung-Feng <mr.chang.jf@gmail.com>
2018-08-13 13:24:04 +0800

    Add backup item: show tech-support

--- bash/bin/traffic/C2960G.sh
+++ bash/bin/traffic/C2960G.sh
@@ -8,7 +8,7 @@ TFTP_HOST=$(ip addr show dev $TFTP_HOST_MAINNIC | grep "inet" | awk 'NR==1{print
 TFTP_DIR="/tftpboot"
 BACKUP_IOS_NAME=$IOS_NAME
 #BACKUP_DIR="/home/angus/Dropbox/@work/20170705_台北市交大維護/`date +%Y%m%d-%H%M%S`_月保養"
-BACKUP_DIR="/home/angus/Dropbox/@work/20170705_新北市交通大隊維護案/`date +%Y%m%d`_月保養"
+BACKUP_DIR="/home/angus/Dropbox/@work/20170705_新北市交通大隊維護案/A.月保養紀錄歸檔/`date +%Y%m%d`_月保養"
 #BACKUP_DIR="/tmp/`date +%Y%m%d-%H%M%S`_月保養"
 
 echo "**IOS backup utility**"
@@ -17,15 +17,18 @@ echo "To $TFTP_HOST into $BACKUP_DIR"
 echo "Telnet to $TARGET_SWITCH and execute below procedure:"
 echo "================================================================================"
 echo "en"
-echo "copy $IOS_LOCATION$IOS_NAME tftp:"
-echo "$TFTP_HOST"
-echo "$BACKUP_IOS_NAME"
-echo "copy nvram:startup-config tftp:"
-echo "$TFTP_HOST"
-echo "C2960_startup-config"
-echo "copy flash:vlan.dat tftp:"
-echo "$TFTP_HOST"
-echo "C2960_vlan.dat"
+echo "copy $IOS_LOCATION$IOS_NAME tftp://10.7.6.143/c2960-lanbasek9-mz.122-44.SE6.bin"
+#echo "$TFTP_HOST"
+#echo "$BACKUP_IOS_NAME"
+#echo "copy nvram:startup-config tftp:"
+echo "copy nvram:startup-config tftp://10.7.6.143/C2960_startup-config"
+#echo "$TFTP_HOST"
+#echo "C2960_startup-config"
+#echo "copy flash:vlan.dat tftp:"
+echo "copy flash:vlan.dat tftp://10.7.6.143/C2960_vlan.dat"
+#echo "$TFTP_HOST"
+#echo "C2960_vlan.dat"
+echo "sh tech-support | redirect tftp://10.7.6.143/C2960_TechSupport.txt"
 echo "quit"
 echo "================================================================================"
 
@@ -39,12 +42,15 @@ if md5sum --status -c $BACKUP_IOS_NAME.md5sum; then
     cp -ab $TFTP_DIR/$BACKUP_IOS_NAME* $BACKUP_DIR/
     cp -ab C2960_startup-config $BACKUP_DIR/
     cp -ab C2960_vlan.dat $BACKUP_DIR/
+    cp -ab C2960_TechSupport.txt $BACKUP_DIR/
     rm -f $TFTP_DIR/$BACKUP_IOS_NAME
     rm -f $TFTP_DIR/C2960_startup-config
     rm -f $TFTP_DIR/C2960_vlan.dat
+    rm -f $TFTP_DIR/C2960_TechSupport.txt
     sudo touch $TFTP_DIR/$BACKUP_IOS_NAME && sudo chmod 777 $TFTP_DIR/$BACKUP_IOS_NAME && sudo chown nobody $TFTP_DIR/$BACKUP_IOS_NAME
     sudo touch $TFTP_DIR/C2960_startup-config && sudo chmod 777 $TFTP_DIR/C2960_startup-config && sudo chown nobody $TFTP_DIR/C2960_startup-config
     sudo touch $TFTP_DIR/C2960_vlan.dat && sudo chmod 777 $TFTP_DIR/C2960_vlan.dat && sudo chown nobody $TFTP_DIR/C2960_vlan.dat
+    sudo touch $TFTP_DIR/C2960_TechSupport.txt && sudo chmod 777 $TFTP_DIR/C2960_TechSupport.txt && sudo chown nobody $TFTP_DIR/C2960_TechSupport.txt
 else
     echo "# The MD5 sum didn't match, require manually troubleshotting"
 fi
--- bash/bin/traffic/C4503.sh
+++ bash/bin/traffic/C4503.sh
@@ -8,7 +8,8 @@ TFTP_HOST=$(ip addr show dev $TFTP_HOST_MAINNIC | grep "inet" | awk 'NR==1{print
 TFTP_DIR="/tftpboot"
 BACKUP_IOS_NAME=$IOS_NAME
 #BACKUP_DIR="/home/angus/Dropbox/@work/20170705_台北市交大維護/`date +%Y%m%d-%H%M%S`_月保養"
-BACKUP_DIR="/home/angus/Dropbox/@work/20170705_新北市交通大隊維護案/`date +%Y%m%d`_月保養"
+BACKUP_DIR="/home/angus/Dropbox/@work/20170705_新北市交通大隊維護案/A.月保養紀錄歸檔/`date +%Y%m%d`_月保養"
+
 #BACKUP_DIR="/tmp/`date +%Y%m%d-%H%M%S`_月保養"
 
 echo "**IOS backup utility**"
@@ -17,15 +18,16 @@ echo "To $TFTP_HOST into $BACKUP_DIR"
 echo "Telnet to $TARGET_SWITCH and execute below procedure:"
 echo "================================================================================"
 echo "en"
-echo "copy $IOS_LOCATION$IOS_NAME tftp:"
-echo "$TFTP_HOST"
-echo "$BACKUP_IOS_NAME"
-echo "copy nvram:startup-config tftp:"
-echo "$TFTP_HOST"
-echo "C4503_startup-config"
-echo "copy cat4000_flash:vlan.dat tftp:"
-echo "$TFTP_HOST"
-echo "C4503_vlan.dat"
+echo "copy $IOS_LOCATION$IOS_NAME tftp:tftp://10.7.6.143/cat4500e-ipbase-mz.122-54.SG1.bin"
+#echo "$TFTP_HOST"
+#echo "$BACKUP_IOS_NAME"
+echo "copy nvram:startup-config tftp://10.7.6.143/C4503_startup-config"
+#echo "$TFTP_HOST"
+#echo "C4503_startup-config"
+echo "copy cat4000_flash:vlan.dat tftp://10.7.6.143/C4503_vlan.dat"
+#echo "$TFTP_HOST"
+#echo "C4503_vlan.dat"
+echo "sh tech-support | redirect tftp://10.7.6.143/C4503_TechSupport.txt"
 echo "quit"
 echo "================================================================================"
 
@@ -39,12 +41,15 @@ if md5sum --status -c $BACKUP_IOS_NAME.md5sum; then
     cp -ab $TFTP_DIR/$BACKUP_IOS_NAME* $BACKUP_DIR/
     cp -ab C4503_startup-config $BACKUP_DIR/
     cp -ab C4503_vlan.dat $BACKUP_DIR/
+    cp -ab C4503_TechSupport.txt $BACKUP_DIR/
     rm -f $TFTP_DIR/$BACKUP_IOS_NAME
     rm -f $TFTP_DIR/C4503_startup-config
     rm -f $TFTP_DIR/C4503_vlan.dat
+    rm -f $TFTP_DIR/C4503_TechSupport.txt
     sudo touch $TFTP_DIR/$BACKUP_IOS_NAME && sudo chmod 777 $TFTP_DIR/$BACKUP_IOS_NAME && sudo chown nobody $TFTP_DIR/$BACKUP_IOS_NAME
     sudo touch $TFTP_DIR/C4503_startup-config && sudo chmod 777 $TFTP_DIR/C4503_startup-config && sudo chown nobody $TFTP_DIR/C4503_startup-config
     sudo touch $TFTP_DIR/C4503_vlan.dat && sudo chmod 777 $TFTP_DIR/C4503_vlan.dat && sudo chown nobody $TFTP_DIR/C4503_vlan.dat
+    sudo touch $TFTP_DIR/C4503_TechSupport.txt && sudo chmod 777 $TFTP_DIR/C4503_TechSupport.txt && sudo chown nobody $TFTP_DIR/C4503_TechSupport.txt
 else
     echo "# The MD5 sum didn't match, require manually troubleshotting"
 fi
